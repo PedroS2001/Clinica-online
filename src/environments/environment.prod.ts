980ab5dc6ec5822c03e6aebf299084e24119e97a
Pedro <pedroseneriz01@gmail.com>
2021-10-20 16:27:08 -0300

    deploy heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAZDsEOwH2oJ_24KhHFiz0C2znvuaRZxMg",
+    authDomain: "clinicaonline-7af65.firebaseapp.com",
+    projectId: "clinicaonline-7af65",
+    storageBucket: "clinicaonline-7af65.appspot.com",
+    messagingSenderId: "793394913195",
+    appId: "1:793394913195:web:7e09abffb3083108d7bcee",
+    measurementId: "G-6GK8HR4CGK"
+  }
 };
